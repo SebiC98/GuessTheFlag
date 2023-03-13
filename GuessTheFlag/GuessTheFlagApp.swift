f1ab003e45970f361b1def3d772584d15426f3cd
SebiC98 <sebastiancioata98@gmail.com>
2023-03-13 08:46:31 +0200

    Guess the flag

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sebastian Cioată on 12.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
